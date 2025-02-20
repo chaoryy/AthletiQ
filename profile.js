65e2ddfec02982c0130fdeb59e3cb998b434c6a9
AminaKadyrkulova <aminakan4@gmail.com>
2025-02-20 15:53:08 +0600

    done profile

---
+++ profile.js
@@ -0,0 +1,63 @@
+
+logoutBtn.addEventListener("click", function () {
+    localStorage.clear();
+    window.location.href = "login.html"; // Перенаправляем на страницу входа
+});        
+
+
+function toggleDropdown(contentId, iconId) {
+    const content = document.getElementById(contentId);
+    const icon = document.getElementById(iconId);
+  
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        content.style.padding = "0 10px";
+        icon.classList.remove("open");
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.padding = "10px 10px";
+        icon.classList.add("open");
+    }
+}
+
+
+/*модальные окна*/
+
+const modal = document.getElementById("recipeModal");
+
+function openRecipe(title, image, description, macros, ingredients, instructions) {
+    const modal = document.getElementById("recipeModal"); 
+    modal.style.display = "flex"; // Показываем окно
+    modal.classList.add("active"); 
+
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalImage").src = image;
+    document.getElementById("modalDescription").innerText = description;
+    document.getElementById("modalCalories").innerText = macros;
+
+    const ingredientsList = document.getElementById("modalIngredients");
+    ingredientsList.innerHTML = ""; 
+    ingredients.split(", ").forEach(ingredient => {
+        const li = document.createElement("li");
+        li.innerText = ingredient;
+        ingredientsList.appendChild(li);
+    });
+
+    document.getElementById("modalInstructions").innerText = instructions;
+}
+
+function closeModal() {
+    const modal = document.getElementById("recipeModal");
+    modal.style.display = "none"; // Скрываем окно
+    modal.classList.remove("active");
+}
+
+document.querySelector(".close").addEventListener("click", closeModal);
+
+window.addEventListener("click", (event) => {
+    const modal = document.getElementById("recipeModal");
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
